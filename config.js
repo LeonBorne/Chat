3684651d495648b6370bc80c28359f048197aac1
Vacant Neo <brawlgems100@gmail.com>
2025-09-08 15:37:11 +0545

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyDXH049L9vlM4NADvKyLuynz_6omTKCtp8",
+  authDomain: "chat-backend-e3889.firebaseapp.com",
+  databaseURL: "https://chat-backend-e3889-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chat-backend-e3889",
+  storageBucket: "chat-backend-e3889.appspot.com",
+  messagingSenderId: "701028221493",
+  appId: "1:701028221493:web:418e64f0a1c5eef067677c",
+  measurementId: "G-C53RD6HQC7"
+};
